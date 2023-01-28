b640f3d1bf117bd709a4ed1fd0cf16197feeb329
victor2905 <98538867+victor2905@users.noreply.github.com>
2023-01-27 22:37:24 -0500

    f_strings
    Creación de proyecto

---
+++ f_strings.py
@@ -0,0 +1,17 @@
+#Ejemplo 1
+
+print(f"El resultado de la suma 4+1 es igual a: {4+1}")
+
+#Ejemplo 2
+nombre = "Carlos"
+estatura = 1.8
+edad = 22
+
+print(f"Hola {nombre} tienes {edad} años y mides {estatura} centímetros")
+
+#Ejemplo 3
+nombre1 = input("¿Cuál es tu nombre?: ")
+num_uno = int(input("Introduce un número: "))
+num_dos = int(input("Introduce un segundo número: "))
+
+print(f"Hola {nombre1} el resultado de {num_uno} + {num_dos} es: {num_uno + num_dos}")
